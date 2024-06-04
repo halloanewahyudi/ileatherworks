0e25497f3abfd10f6b5f0cc06ad78baf1370c292
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2024-06-04 14:58:55 +0700

    logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { useAuthStore } from "@/stores/authStore";
+
 import Unlock from '@/components/icons/Unlock.vue';
+import { useRouter } from "vue-router";
+const router = useRouter()
 const auth = useAuthStore()
+const logout= () => {
+    router.push('/login').then(()=>{
+        router.go(0)
+    })
+}
 </script>
 <template>
     <div class="flex justify-between items-center gap-4">
@@ -11,7 +19,7 @@ const auth = useAuthStore()
         <div class="menu flex items-center gap-4">
             <router-link to="/"> Dashboard </router-link>
             <router-link to="/print"> Print </router-link>
-            <button
+            <button @click="logout"
                 class="py-2 px-4 rounded-full bg-brand-600 text-brand-50 hover:bg-brand-800 duration-300 flex items-center gap-2">
                 Logout
                 <Unlock />
